81b653a912e17cd6c30fff7d65a60e51fd22e3f9
vCesar <71222384+vCesar1mx@users.noreply.github.com>
2022-05-22 23:34:46 -0500

    Update Main.java

--- me.vcesar.playerinfo/src/me/vcesar/playerinfo/Main.java
+++ me.vcesar.playerinfo/src/me/vcesar/playerinfo/Main.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package me.vcesar.playerinfo;
 import java.io.IOException;
 import org.bukkit.Bukkit;
